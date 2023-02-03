32071c24e432a11102f7268c1d9e266f9e35a62d
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-02-03 19:46:19 +0600

    first commit

---
+++ array-push-pop.js
@@ -0,0 +1,14 @@
+// push,,,,,,
+
+const fruits = ['mango', 'banana','orange'];
+
+console.log(fruits);
+const total = fruits.push('watermelon', 'apple');
+
+console.log(fruits);
+console.log(total);
+
+// Pop,,,,,,,
+
+const popped = fruits.pop('apple');
+console.log(fruits);
